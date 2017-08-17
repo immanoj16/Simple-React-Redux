b427b743a6f86ca0fe5ff44e5152129bc64ffb55
immanoj16 <mmaharana14@gmail.com>
2017-08-17 14:37:19 +0530

    Add: event handler

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const SearchBar = () => {
-  return <input type="text"/>
-};
+class SearchBar extends Component {
+
+  onInputChange(e) {
+
+  }
+
+  render () {
+    return (
+      <input type="text" onChange={(e) => console.log(e.target.value)}/>
+    )
+  }
+}
 
 export default SearchBar
