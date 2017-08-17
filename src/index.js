f6fd6526184108e002e8802a3f56d5328430af9f
immanoj16 <mmaharana14@gmail.com>
2017-08-17 14:26:55 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import SearchBar from './components/SearchBar'
+
+const API_KEY = 'AIzaSyApv6QTaTn3a5ZallMxHecMhtPku9SbvGg'
+
+
+ReactDOM.render(
+  <SearchBar/>,
+  document.getElementById('app')
+)
