8619a53edb840b3baf585fbb6734fd11623eb3e1
immanoj16 <mmaharana14@gmail.com>
2017-08-17 18:31:19 +0530

    Add: current playing video

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -2,19 +2,21 @@ import React from 'react'
 
 const VideoDetail = ({video}) => {
 
-  const { title, description } = video.snippet
+  if (!video) {
+    return <div>Loading...</div>
+  }
 
   const videoId = video.id.videoId
   const url = `https://www.youtube.com/embed/${videoId}`
 
   return (
-    <div className="video-detail col-md-8">
-      <div className="embed-responsive embed-responsive-16by9">
-        <iframe src={url} frameborder="0" className="embed-responsive-item"></iframe>
+    <div className='video-detail col-md-8'>
+      <div className='embed-responsive embed-responsive-16by9'>
+        <iframe src={url} className='embed-responsive-item' />
       </div>
-      <div className="details">
-        <div>{title}</div>
-        <div>{description}</div>
+      <div className='details'>
+        <div>{video.snippet.title}</div>
+        <div>{video.snippet.description}</div>
       </div>
     </div>
   )
